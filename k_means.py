adc6dd16feb67f8602cff3fcc5203aa913dc1dbe
AmeerAliSaleem <116832927+AmeerAliSaleem@users.noreply.github.com>
2025-04-22 13:23:54 +0100

    Updated code to work for n-dimensional arrays
    Also added docstrings

--- k_means.py
+++ k_means.py
@@ -3,7 +3,23 @@ import numpy as np
 # An implementation of the K-Means algorithm from scratch.
 # Thanks to Emma Ding's YT video for helping me write the code for this class: https://youtu.be/uLs-EYUpGAw?si=N-uPPx6cty5af7Ey
 class KMeans:
+    """
+    A class for performing K-Means clustering on a dataset. The data is expected to be in the form
+    of a list of NumPy ndarrays, where each ndarray represents the coordinates of a data point.
+    """
+    
     def __init__(self, k=3, max_iter=100, tol=0.001):
+        """
+        Parameters
+        ----------
+        k : int, optional
+            The number of clusters to form (default is 3).
+        max_iter : int, optional
+            The maximum number of iterations to run the algorithm for (default is 100).
+        tol : float, optional
+            The tolerance level for the stopping criteria (default is 0.001).
+        """
+
         self.k = k
         self.max_iter = max_iter
         self.tol = tol
@@ -11,37 +27,69 @@ class KMeans:
     def euclidean_distance(self, point1, point2):
         """
         Computes the Euclidean distance between the pair of input points.
+
+        Parameters
+        ----------
+        point1 : np.ndarray
+            The first point data point
+        point2 : np.ndarray
+            The second data point.
+
+        Returns
+        ----------
+        distance : float
+            The Euclidean distance between the two points.
         """
 
-        return np.linalg.norm(point1-point2)
+        distance = np.linalg.norm(point1-point2)
+        return distance
     
     def initialise_centroids(self, data):
         """
         Initialises the centroids' coordinates randomly within the data range.
         If we just took k random points from the data, the initialisation will be biased
-        if the data is not uniformly spread across the data space.
+        if the data is not uniformly spread across the data space, hence why we instead
+        implement this 'bounding box' method.
+
+        Parameters
+        ----------
+        data : list
+            List of NumPy ndarrays. Each array represents the coordinates of a data point.
+
+        Returns
+        ----------
+        centroids : list
+            List of NumPy ndarrays. Each array represents the coordinates of a randomly initialised centroid.
         """
 
-        # separate x and y coordinates
-        data_x = np.array([point[0] for point in data])
-        data_y = np.array([point[1] for point in data])
+        # group together coordinates per dimension
+        stack = np.stack(data, axis=1)
 
-        x_min = np.min(data_x)
-        x_max = np.max(data_x)
-        y_min = np.min(data_y)
-        y_max = np.max(data_y)
-
-        data_min = np.array([x_min, y_min])
-        data_max = np.array([x_max, y_max])
+        # create arrays to store the minimum/maximum values in each coordinate dimension
+        data_min = np.min(stack, axis=1)
+        data_max = np.max(stack, axis=1)
 
         # generate k random centroids within the data range
-        centroids = [data_min + np.random.uniform() * (data_max - data_min) for _ in range(self.k)]
+        # (this is more robust than just taking k random points from the data)
+        centroids = [data_min + np.random.uniform()*(data_max - data_min) for _ in range(self.k)]
 
         return centroids
     
     def get_labels(self, data, centroids):
         """
         Assigns each data point to the nearest centroid.
+
+        Parameters
+        ----------
+        data : list
+            List of NumPy ndarrays. Each array represents the coordinates of a data point.
+        centroids : list
+            List of NumPy ndarrays. Each array represents the coordinates of a centroid.
+
+        Returns
+        ----------
+        labels : list
+            List of integers. Each integer represents the cluster whose centroid each data point is closest to.
         """
 
         labels = []
@@ -61,7 +109,19 @@ class KMeans:
 
     def update_centroids(self, data, labels):
         """
-        Updates the centroids' coordinates based on the current labels
+        Updates the centroids' coordinates based on the current labels.
+
+        Parameters
+        ----------
+        data : list
+            List of NumPy ndarrays. Each array represents the coordinates of a data point.
+        labels : list
+            List of integers. Each integer represents the cluster whose centroid each data point is closest to.
+
+        Returns
+        ----------
+        new_centroids : list
+            List of NumPy ndarrays. Each array represents the updated coordinates of a centroid.
         """
 
         new_centroids = []
@@ -79,6 +139,19 @@ class KMeans:
         Checks whether the algorithm should terminate at the current iteration.
         This happens if either: the total centroid deviations do not exceed the tolerance level;
         or the maximum number of iterations has been reached.
+
+        Parameters
+        ----------
+        old_centroids : list
+            List of NumPy ndarrays. Each array represents the previous coordinates of a centroid.
+        new_centroids : list
+            List of NumPy ndarrays. Each array represents the new coordinates of a centroid.
+        current_iter : int
+            The current iteration of the algorithm.
+
+        Returns
+        ----------
+        boolean representing whether the algorithm should terminate (True) or not (False).
         """
         
         total_deviation = 0
@@ -93,26 +166,55 @@ class KMeans:
     
     def fit(self, data):
         """
-        Applies the K-Means algorithm to the input data.
+        Applies the K-Means algorithm to the input data. The cluster centroids and data point labels
+        are stored in lists. The i-th element of each list corresponds to the labellings/centroid allocations 
+        at the i-th iteration of the algorithm.
+
+        Parameters
+        ----------
+        data : np.ndarray
+            The input data to perform the clustering on.
+
+        Returns
+        ----------
+        None, although the following attributes are set:
+        self.labels : list
+            The labels assigned to each data point. Each element in the listcorresponds to the 
+            labellings at the i-th iteration of the algorithm.
+        self.centroids : list
+            The coordinates of the centroids. Each element in the list corresponds to the 
+            centroid allocations at the i-th iteration of the algorithm.
         """
 
+        # initialise output lists
+        i = 0
+        labels, centroids = [], []
+
         # initialise centroids randomly within the data range
-        centroids = self.initialise_centroids(data)
+        centroids.append(self.initialise_centroids(data))
+
+        # initialise labels based on the centroid initialisations
+        labels.append(self.get_labels(data, centroids[0]))
 
         # main loop of algorithm
-        current_iter = 0
         while True:
-            old_centroids = centroids
-            labels = self.get_labels(data, centroids)
-            centroids = self.update_centroids(data, labels)
+            i += 1
 
-            current_iter += 1
+            # update centroids and labels
+            old_centroids = centroids[i-1]
+            new_centroids = self.update_centroids(data, labels[i-1])
+            new_labels = self.get_labels(data, new_centroids)
+
+            # store new centroids and new labels
+            centroids.append(new_centroids)
+            labels.append(new_labels)
+            
 
             # check stopping criteria
-            stopping_criteria = self.stopping_criteria(old_centroids, centroids, current_iter)
+            stopping_criteria = self.stopping_criteria(old_centroids, new_centroids, i)
             if stopping_criteria:
                 break
 
-        # store labels and centroids
+        # store lists for labels and centroids
         self.labels = labels
         self.centroids = centroids
